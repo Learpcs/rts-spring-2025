dc5d2e8e4b67f82720aeefd6416eb307c907b5c7
Learpcs <34576126+Learpcs@users.noreply.github.com>
2025-04-03 17:05:30 +0400

    Update task03.ino

--- tasks/task03/task03.ino
+++ tasks/task03/task03.ino
@@ -1,55 +1,55 @@
-volatile unsigned long previousMicros = 0;
-volatile unsigned long totalInterval = 0;
-volatile unsigned long totalSquaredInterval = 0;
-volatile unsigned int sampleCount = 0;
-const unsigned int maxSampleSize = 1000;
+volatile unsigned long long previousMicros = 0;
+volatile unsigned long long totalInterval = 0;
+volatile unsigned long long totalSquaredInterval = 0;
+volatile unsigned long long sampleCount = 0;
+const unsigned long long maxSampleSize = 1000;
 
 void setup() {
-  Serial.begin(115200);
-  pinMode(21, INPUT);
-  attachInterrupt(digitalPinToInterrupt(21), recordInterval, CHANGE);
-}
+  Serial.begin(9600);
+  pinMode(3, INPUT);
+  attachInterrupt(digitalPinToInterrupt(3), recordInterval, HIGH);
+ }
 
 void loop() {
   if (sampleCount >= maxSampleSize) {
     noInterrupts();
-    unsigned long sumIntervals = totalInterval;
-    unsigned long sumSquaredIntervals = totalSquaredInterval;
-    unsigned int totalSamples = sampleCount;
+    unsigned long long sumIntervals = totalInterval;
+    unsigned long long sumSquaredIntervals = totalSquaredInterval;
+    unsigned long long totalSamples = sampleCount;
     totalInterval = 0;
     totalSquaredInterval = 0;
-    sampleCount = 0;
+    sampleCount = 0;   
     interrupts();
+   
 
-    unsigned long average = sumIntervals / totalSamples;
-    unsigned long variance = (sumSquaredIntervals / totalSamples) - (average * average);
-    unsigned long standardDeviation = sqrt(variance);
+    float average = sumIntervals / totalSamples;
+    float variance = (sumSquaredIntervals / totalSamples) - (average * average);
+    unsigned long long standardDeviation = sqrt(variance);
 
     Serial.print("Average: ");
-    Serial.print(average);
+    Serial.print((float)average);
     Serial.print(" us, Total Squared: ");
-    Serial.print(sumSquaredIntervals);
+    Serial.print((float)sumSquaredIntervals);
     Serial.print(" us, Total Samples: ");
-    Serial.print(totalSamples);
+    Serial.print((float)totalSamples);
     Serial.print(" us, Average^2: ");
-    Serial.print(average * average);
+    Serial.print((float)(average * average));
     Serial.print(" us, Variance: ");
-    Serial.print(variance);
+    Serial.print((float)variance);
     Serial.print(" us, StdDev: ");
-    Serial.print(standardDeviation);
+    Serial.print((float)standardDeviation);
     Serial.println(" us");
+
   }
 }
 
 void recordInterval() {
-  unsigned long currentMicros = micros();
-  if (previousMicros) {
-    unsigned long interval = currentMicros - previousMicros;
+  unsigned long long currentMicros = micros();
+  unsigned long long interval = currentMicros - previousMicros;
 
-    totalInterval += interval;
-    totalSquaredInterval += interval * interval;
-    sampleCount++;
-  }
+  totalInterval += interval;
+  totalSquaredInterval += interval * interval;
+  sampleCount++;
 
   previousMicros = currentMicros;
 }
